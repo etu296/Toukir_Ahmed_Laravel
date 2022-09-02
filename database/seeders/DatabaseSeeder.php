eff3f696e5929595109041b3b563f75cbb825609
etu269 <etumahmuda@gmail.com>
2022-09-02 16:01:05 +0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call([AchivementSeeder::class]); 
+        $this->call([AchivementSeeder::class]); 
         $this->call([AdminSeeder::class]); 
 
         // \App\Models\User::factory(10)->create();
